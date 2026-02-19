2ec60661cd8b89386fc16433b13d4fbd6e47d41f
pratham1c1 <prathamchandra2001@gmail.com>
2026-02-19 19:02:56 +0530

    adding ActivateProfile for tests

--- ecomOrderService/src/test/java/com/pcProject/ecomOrderService/EcomOrderServiceApplicationTests.java
+++ ecomOrderService/src/test/java/com/pcProject/ecomOrderService/EcomOrderServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.pcProject.ecomOrderService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcomOrderServiceApplicationTests {
 
 	@Test
--- ecomServiceRegistry/src/test/java/com/pcProject/ecomServiceRegistry/EcomServiceRegistryApplicationTests.java
+++ ecomServiceRegistry/src/test/java/com/pcProject/ecomServiceRegistry/EcomServiceRegistryApplicationTests.java
@@ -2,8 +2,10 @@ package com.pcProject.ecomServiceRegistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcomServiceRegistryApplicationTests {
 
 	@Test
